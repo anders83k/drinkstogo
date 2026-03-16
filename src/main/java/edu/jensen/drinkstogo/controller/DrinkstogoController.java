61de00caa78eefef7170cc761a3d2e708fdbf30b
ammarovehub <ammarove1@protonmail.com>
2026-03-16 01:17:03 +0100

    fix: resolve test discovery issue by adding JUnit BOM
    - Added JUnit BOM to manage dependency versions consistently.
    - Removed explicit dependencies on junit-jupiter-api and junit-jupiter-engine.
    - Moved DrinkstogoApplicationTests to src/test/java directory for proper test discovery.
    - These changes ensure that the test phase runs successfully without conflicts.

---
+++ src/main/java/edu/jensen/drinkstogo/controller/DrinkstogoController.java
@@ -0,0 +1,48 @@
+package edu.jensen.drinkstogo.controller;
+
+import edu.jensen.drinkstogo.dto.DrinkstogoDTO;
+import edu.jensen.drinkstogo.service.DrinkstogoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/drinkstogo")
+public class DrinkstogoController {
+    private final DrinkstogoService drinkstogoService;
+
+    public DrinkstogoController(DrinkstogoService drinkstogoService) {
+        this.drinkstogoService = drinkstogoService;
+    }
+
+    @GetMapping
+    public List<DrinkstogoDTO> getAllDrinks(){      //eller List<DrinkstogoDTO> alt: getDrinkByID(){
+        //fetchalldrinks(); rad 24 & 25 alt: drinkstogoService.getDrinkById(2); plockas bort om man kör list
+        return drinkstogoService.getAllDrinks();        //getDrinkById() ex: getDrinkById(2) ;
+    }
+    @GetMapping ("/{id}")
+    public DrinkstogoDTO getDrinkById(@PathVariable Integer id){
+        return drinkstogoService.getDrinkById(id);
+    }
+    @PostMapping
+    public DrinkstogoDTO createDrink(@RequestBody DrinkstogoDTO dto){
+        return drinkstogoService.saveDrink(dto);
+    }
+    @DeleteMapping ("/{id}")
+    public void deleteDrink(@PathVariable Integer id) {
+        drinkstogoService.deleteDrinkById(id);
+    }
+    @PutMapping ("/{id}")
+    public DrinkstogoDTO updateDrink(@PathVariable Integer id, @RequestBody DrinkstogoDTO dto){
+        return drinkstogoService.updateDrink(id, dto);
+    }
+<<<<<<< HEAD:src/main/java/edu/jensen/drinkstogo/controller/DrinkstogoController.java
+
+
+}
+=======
+
+
+}
+
+>>>>>>> ccf6bb030b85c291c55e7f6ad5aee32b232a107a:DrinkstogoController.java
